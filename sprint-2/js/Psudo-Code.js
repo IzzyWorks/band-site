90a75f0056ba32c3e7d056f5b896d71a0cab4f2b
IzzyWorks <60447338+IzzyWorks@users.noreply.github.com>
2021-01-28 12:56:06 -0800

    sprint 2 push plus new js files

---
+++ sprint-2/js/Psudo-Code.js
@@ -0,0 +1,10 @@
+// Add a series of <li> elements into a <ul> container using a for-loop
+// determin the amount of <li> elements to add by looking up the length of an array
+// Give each <li> element a universal style
+// Populate each <li> with three <h2> elements and one <a> element
+// Add 4 unique classes, to each nth child element nested insite the <li> container
+// Repeate this action for ever <li> child of <ul> container
+// Populate each <h2> element with a string of text, found in a object nested in an array
+// Add a universal class to <a> element
+// Add a universal sting of text to <a> element
+// Add a unique link path to each <a> element, found in a object nested in an array
