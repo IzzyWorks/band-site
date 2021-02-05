c3b510169a28cb9da046576b11e38551221d473f
IzzyWorks <60447338+IzzyWorks@users.noreply.github.com>
2021-02-05 11:40:01 -0800

    setting up git and push axios

---
+++ sprint-3/js/date-format.js
@@ -0,0 +1,37 @@
+'use strict';
+
+console.log('dates');
+
+
+
+function isToday(event) {
+    event.true {
+
+    }
+
+
+
+let isToday = true;
+let isYesterday = false;
+
+if (isToday = true) {
+    console.log("Posted today");
+} else if (isToday === false && isYesterday === True) {
+    console.log("Posted yesterday");
+} else {
+    console.log(Date());
+};
+
+
+
+const whatIsTheDate = (someDate) => {
+    const today = new Date()
+    return someDate.getDate() === today.getDate() &&
+      someDate.getMonth() === today.getMonth() &&
+      someDate.getFullYear() === today.getFullYear()
+    const yesterday = new Date()
+  }
+
+
+
+
